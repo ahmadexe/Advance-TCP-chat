8a5a0befb38bd9b106a5fe1ccf37555739fcddc3
Ahmad <muahmad710@gmail.com>
2023-01-11 11:53:21 +0500

    broadcast and message handles done

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+        
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+        
