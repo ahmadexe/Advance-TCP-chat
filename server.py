2b7a1fe89a75fc034776f45c28dfe8590e1b6cc7
Ahmad <muahmad710@gmail.com>
2023-01-11 11:55:38 +0500

    broadcast and message handles done

--- server.py
+++ server.py
@@ -29,3 +29,22 @@ def handle(client):
             nicknames.remove(nickname)
             break
         
+def receive():
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+        
+print("Server is live")
+receive()
